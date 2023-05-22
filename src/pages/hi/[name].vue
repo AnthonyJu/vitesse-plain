172b8cdc01c9f87bf7769fff22dacc4b8a584619
鞠鹏 <1662345805@qq.com>
2023-05-22 19:27:31 +0800

    Update [name].vue

--- src/pages/hi/[name].vue
+++ src/pages/hi/[name].vue
@@ -2,7 +2,7 @@
   <div flex-col-center>
     <div i-carbon:pedestrian text-4xl />
     <p>hi,{{ name }}</p>
-    <p text-sm opacity-75>动态路由演示</p>swdadd xxee
+    <p text-sm opacity-75>动态路由演示</p>
 
     <template v-if="user.otherNames.length">
       <p mt-4 text-sm>
