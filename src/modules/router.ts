ddfba3026f78be0b0a0f96ae1d14eff2470d82de
JuPeng <1662345805@qq.com>
2023-11-04 22:36:21 +0800

    Add whitelist for router navigation

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -7,4 +7,12 @@ const routes = setupLayouts(pages)
 
 export const router = createRouter({ routes, history: createWebHashHistory() })
 
+const WhiteList: string[] = [] // TODO 路由白名单
+
+router.beforeEach(async (to, from, next) => {
+  if (WhiteList.includes(to.path)) next()
+  // TODO 其他路由权限判断
+  else next()
+})
+
 export default (app: App) => app.use(router)
