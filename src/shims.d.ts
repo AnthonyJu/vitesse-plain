33cbad4c07ecd131cd1227e8a583add17f003959
JuPeng <1662345805@qq.com>
2023-11-04 21:25:03 +0800

    Remove unnecessary module declarations in shims.d.ts.

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -3,16 +3,6 @@ declare interface Window {
   [key: string]: any
 }
 
-declare module 'element-plus/dist/locale/zh-cn.mjs'
-
-// with unplugin-vue-markdown, markdown files can be treated as Vue components
-declare module '*.md' {
-  import type { DefineComponent } from 'vue'
-
-  const component: DefineComponent<object, object, any>
-  export default component
-}
-
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
 
