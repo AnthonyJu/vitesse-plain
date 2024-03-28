ad32b9e6d5830477eecdf26ba37796f2d9e446a5
AnthonyJu <1662345805@qq.com>
2024-03-28 22:49:41 +0800

    pref: vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import path from 'node:path'
-import process from 'node:process'
 import { defineConfig } from 'vite'
 import VueRouter from 'unplugin-vue-router/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
@@ -13,150 +12,152 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 import Unocss from 'unocss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig({
-  server: {
-    hmr: true,
-    host: true,
-    open: true,
-    port: 8080,
-    proxy: {},
-  },
-
-  resolve: {
-    alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`,
+export default defineConfig(({ command }) => {
+  return {
+    server: {
+      hmr: true,
+      host: true,
+      open: true,
+      port: 8080,
+      proxy: {},
     },
-  },
 
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "@/styles/element/index.scss" as *;',
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`,
       },
     },
-  },
 
-  optimizeDeps: {
-    include: [
-      '@element-plus/icons-vue',
-      '@vueuse/core',
-      'axios',
-      'element-plus/es',
-      'element-plus/es/components/base/style/index',
-      'element-plus/es/components/message/style/index',
-      'element-plus/es/components/message-box/style/index',
-      'element-plus/es/components/notification/style/index',
-      'unplugin-vue-router/runtime',
-      'unplugin-vue-router/data-loaders/basic',
-    ],
-  },
-
-  plugins: [
-    // https://github.com/posva/unplugin-vue-router
-    VueRouter({
-      extensions: ['.vue'],
-      routeBlockLang: 'yaml',
-      dts: 'src/typed-router.d.ts',
-      exclude: ['**/components/**/*'],
-    }),
-
-    // https://github.com/vitejs/vite-plugin-vue/tree/main/packages/plugin-vue#readme
-    Vue({
-      include: [/\.vue$/],
-      script: {
-        propsDestructure: true,
-      },
-    }),
-
-    // https://github.com/AnthonyJu/npm-packages/tree/main/packages/vite-plugin-vue-setup-name-support
-    SupportSetupName(),
-
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: [
-        'vue',
-        'pinia',
-        '@vueuse/core',
-        VueRouterAutoImports,
-        {
-          // add any other imports you were relying on
-          'vue-router/auto': ['useLink'],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/styles/element/index.scss" as *;',
         },
-      ],
-      resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
-      dts: 'src/auto-imports.d.ts',
-      dirs: [
-        'src/hooks/**/*',
-        'src/events',
-        'src/stores',
-        'src/utils',
-      ],
-      vueTemplate: true,
-    }),
-
-    // https://github.com/antfu/unplugin-vue-components
-    Components({
-      dts: 'src/components.d.ts',
-      include: [/\.vue$/, /\.vue\?vue/],
-      exclude: ['src/components/**/components/**/*'],
-      // 生产环境下按需引入element-plus
-      resolvers: process.env.NODE_ENV === 'production' ? ElementPlusResolver({ importStyle: 'sass' }) : undefined,
-    }),
-
-    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
-    Layouts(),
-
-    // 开发环境完整引入element-plus
-    {
-      name: 'vite:element-plus-auto-import-in-dev',
-      transform(code, id) {
-        if (process.env.NODE_ENV === 'development' && /src\/main.ts$/.test(id)) {
-          return {
-            code: `
-              import ElementPlus from 'element-plus'
-              import 'element-plus/theme-chalk/src/index.scss'
-              ${code.split('const app = createApp(App)').join('const app = createApp(App);app.use(ElementPlus);')};
-            `,
-            map: null,
-          }
-        }
       },
     },
 
-    // https://github.com/antfu/unocss
-    // see uno.config.ts for config
-    Unocss(),
+    optimizeDeps: {
+      include: [
+        '@element-plus/icons-vue',
+        '@vueuse/core',
+        'axios',
+        'element-plus/es',
+        'element-plus/es/components/base/style/index',
+        'element-plus/es/components/message/style/index',
+        'element-plus/es/components/message-box/style/index',
+        'element-plus/es/components/notification/style/index',
+        'unplugin-vue-router/runtime',
+        'unplugin-vue-router/data-loaders/basic',
+      ],
+    },
 
-    // https://devtools-next.vuejs.org/guide/vite-plugin
-    VueDevTools(),
+    plugins: [
+      // https://github.com/posva/unplugin-vue-router
+      VueRouter({
+        extensions: ['.vue'],
+        routeBlockLang: 'yaml',
+        dts: 'src/typed-router.d.ts',
+        exclude: ['**/components/**/*'],
+      }),
 
-    // https://github.com/antfu/vite-plugin-pwa
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
-      manifest: {
-        name: 'vitesse plain',
-        short_name: 'vitesse plain',
-        theme_color: '#ffffff',
-        icons: [
+      // https://github.com/vitejs/vite-plugin-vue/tree/main/packages/plugin-vue#readme
+      Vue({
+        include: [/\.vue$/],
+        script: {
+          propsDestructure: true,
+        },
+      }),
+
+      // https://github.com/AnthonyJu/npm-packages/tree/main/packages/vite-plugin-vue-setup-name-support
+      SupportSetupName(),
+
+      // https://github.com/antfu/unplugin-auto-import
+      AutoImport({
+        imports: [
+          'vue',
+          'pinia',
+          '@vueuse/core',
+          VueRouterAutoImports,
           {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
+            // add any other imports you were relying on
+            'vue-router/auto': ['useLink'],
           },
         ],
+        resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
+        dts: 'src/auto-imports.d.ts',
+        dirs: [
+          'src/hooks/**/*',
+          'src/events',
+          'src/stores',
+          'src/utils',
+        ],
+        vueTemplate: true,
+      }),
+
+      // https://github.com/antfu/unplugin-vue-components
+      Components({
+        dts: 'src/components.d.ts',
+        include: [/\.vue$/, /\.vue\?vue/],
+        exclude: ['src/components/**/components/**/*'],
+        // 生产环境下按需引入element-plus
+        resolvers: command === 'build' ? ElementPlusResolver({ importStyle: 'sass' }) : undefined,
+      }),
+
+      // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+      Layouts(),
+
+      // 开发环境完整引入element-plus
+      {
+        name: 'vite:element-plus-auto-import-in-dev',
+        transform(code, id) {
+          if (command === 'serve' && /src\/main.ts$/.test(id)) {
+            return {
+              code: `
+                import ElementPlus from 'element-plus'
+                import 'element-plus/theme-chalk/src/index.scss'
+                ${code.split('const app = createApp(App)').join('const app = createApp(App);app.use(ElementPlus);')};
+              `,
+              map: null,
+            }
+          }
+        },
       },
-    }),
-  ],
+
+      // https://github.com/antfu/unocss
+      // see uno.config.ts for config
+      Unocss(),
+
+      // https://devtools-next.vuejs.org/guide/vite-plugin
+      VueDevTools(),
+
+      // https://github.com/antfu/vite-plugin-pwa
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.svg'],
+        manifest: {
+          name: 'vitesse plain',
+          short_name: 'vitesse plain',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: '/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: '/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+    ],
+  }
 })
