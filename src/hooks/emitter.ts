257edaeaea97e116e3ca528bb602d94878a177dc
AnthonyJu <1662345805@qq.com>
2024-04-12 22:48:34 +0800

    Refactor emitter hooks and types

--- src/hooks/emitter.ts
+++ src/hooks/emitter.ts
@@ -1,19 +1,29 @@
 import mitt from 'mitt'
-
-type Handler<T = unknown> = (event: T) => void
-
-// eslint-disable-next-line ts/consistent-type-definitions
-type Events = {
-  Event_Name: string
-}
+import type { Emitter as EmitterType, EventHandlerMap, Handler } from 'mitt'
 
 // https://github.com/developit/mitt
-export const Emitter = mitt<Events>()
 
-export function useEmitter<Key extends keyof Events>(name: Key, handler: Handler<Events[Key]>) {
+export const Emitter = mittWithOnce<EmitterEvents>()
+
+export function useEmitter<Key extends keyof EmitterEvents>(
+  name: Key,
+  handler: Handler<EmitterEvents[Key]>,
+) {
   Emitter.on(name, handler)
 
   onUnmounted(() => {
     Emitter.off(name, handler)
   })
+}
+
+function mittWithOnce<Events extends EmitterEvents>(all?: EventHandlerMap<Events>) {
+  const mitter = mitt(all)
+  // @ts-expect-error - this is a hack to make the emitter work once
+  mitter.once = function (name, handle) {
+    mitter.on(name, handle)
+    mitter.on(name, mitter.off.bind(mitter, name, handle))
+  }
+  return mitter as unknown as {
+    once: <Key extends keyof Events>(name: Key, handler: Handler<Events[Key]>) => void
+  } & EmitterType<Events>
 }
---
+++ src/types/emitter.d.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line ts/consistent-type-definitions
+type EmitterEvents = {
+  Event_Name: string
+}
