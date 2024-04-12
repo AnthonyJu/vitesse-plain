1679d5641c403fed2ec70c07b3ce63dae86334b0
AnthonyJu <1662345805@qq.com>
2024-04-12 22:48:24 +0800

    Enable uno preset and attribute preset in uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -20,16 +20,22 @@ export default defineConfig({
     'icon-btn': 'color-blue inline-block cursor-pointer select-none transition duration-200 ease-in-out hover:opacity-100 hover:text-green-600',
   },
   presets: [
+    // 启用 uno 预设
     presetUno(),
+    // 启用 属性 预设
     presetAttributify(),
+    // 启用 icons 预设
     presetIcons({
       scale: 1.2,
       warn: true,
     }),
+    // 启用 排版 预设
     presetTypography(),
   ],
   transformers: [
+    // 启用 @apply 功能
     transformerDirectives(),
+    // 启用 () 分组功能，hover:(bg-blue-500 font-large) font-(dark 900)
     transformerVariantGroup(),
   ],
 })
