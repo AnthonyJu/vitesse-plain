646307795332fae491f5dd411268c0d5ac24549d
AnthonyJu <1662345805@qq.com>
2024-04-13 13:42:55 +0800

    Refactor emitter hooks and types, and add support for once and emitRes in mittPro

--- src/hooks/emitter.ts
+++ src/hooks/emitter.ts
@@ -1,9 +1,9 @@
 import mitt from 'mitt'
-import type { Emitter as EmitterType, EventHandlerMap, Handler } from 'mitt'
+import type { Emitter as EmitterType, EventHandlerList, EventType, Handler } from 'mitt'
 
 // https://github.com/developit/mitt
 
-export const Emitter = mittWithOnce<EmitterEvents>()
+export const Emitter = mittPro<EmitterEvents>()
 
 export function useEmitter<Key extends keyof EmitterEvents>(
   name: Key,
@@ -16,14 +16,27 @@ export function useEmitter<Key extends keyof EmitterEvents>(
   })
 }
 
-function mittWithOnce<Events extends EmitterEvents>(all?: EventHandlerMap<Events>) {
-  const mitter = mitt(all)
+// 支持 once 和 emitRes 的 mitt
+function mittPro<Events extends EmitterEvents>() {
+  const mitter = mitt()
   // @ts-expect-error - this is a hack to make the emitter work once
   mitter.once = function (name, handle) {
     mitter.on(name, handle)
     mitter.on(name, mitter.off.bind(mitter, name, handle))
   }
+
+  // @ts-expect-error - this is a hack to make the emitter have a return value
+  mitter.emitRes = <Key extends keyof Events>(type: Key, evt: Events[Key]): any => {
+    const handlers = mitter.all!.get(type as EventType)
+    if (handlers) {
+      return (handlers as EventHandlerList<Events[keyof Events]>)
+        .slice()
+        .map(handler => handler(evt!))
+    }
+  }
+
   return mitter as unknown as {
     once: <Key extends keyof Events>(name: Key, handler: Handler<Events[Key]>) => void
+    emitRes: <Key extends keyof Events>(type: Key, evt: Events[Key]) => any
   } & EmitterType<Events>
 }
